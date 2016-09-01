d061ec82c789f3853498cfff4461580f1cb809d4
Samit <samitkoyom@gmail.com>
2016-09-02 00:48:55 +0700

    first track

---
+++ www/js/index.js
@@ -0,0 +1,17 @@
+var app = {
+
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        
+    },
+
+    onDeviceReady: function() {
+        
+    },
+
+};
+
+app.initialize();
